aac4f8e97d4cc822cda599c6dbe7c42c09f43244
Ayush Siloiya <85118674+ayushsiloiya619@users.noreply.github.com>
2023-03-31 00:16:38 +0530

    file

---
+++ Best_must_try_2.0/best2.cpp
@@ -0,0 +1,10 @@
+//Half of the number .
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    cin>>x;
+    float y=(float)x;
+    cout<<y/2<<endl;//The value u get from here will be a int type.
+    return 0;
+}
