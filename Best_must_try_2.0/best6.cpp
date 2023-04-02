c1fd9fbeac67ac33edfc20b2cbf3b95d3b0f96d4
Ayush Siloiya <85118674+ayushsiloiya619@users.noreply.github.com>
2023-04-03 00:10:20 +0530

    file

---
+++ Best_must_try_2.0/best6.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+    cout<<pow(2,3)<<endl;
+    cout<<sqrt(24)<<endl;
+    int ans=sqrt(24);
+    cout<<ans<<endl;
+    double ans1=sqrt(24);
+    cout<<ans1<<endl;
+    return 0;
+}
+//using the built-in standard library functions in c++.
