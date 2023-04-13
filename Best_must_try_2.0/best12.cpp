272a0ff590265f60562d2fbf9a890fbce15121f7
Ayush Siloiya <85118674+ayushsiloiya619@users.noreply.github.com>
2023-04-13 12:14:03 +0530

    file

---
+++ Best_must_try_2.0/best12.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    //char n;
+    //int a=2;
+    // condition? true : false;
+    //(n>=33) ? cout<<"Pass" : cout<<"Fail";//Ninja technique.
+    if(n>=33) cout<<"Pass";
+    else cout<<"Fail";
+    //n=(a>0) ? '5' : '3';
+    //cout<<"The value of x is: "<<n<<endl;
+    return 0;
+}
+//Sample Input: 45
+// Output: Pass
