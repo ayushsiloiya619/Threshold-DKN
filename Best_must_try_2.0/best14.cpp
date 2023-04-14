fbc2bfa70ea7a7a0f940dd4605ec8c6efb45b49b
Ayush Siloiya <85118674+ayushsiloiya619@users.noreply.github.com>
2023-04-14 08:37:01 +0530

    file

---
+++ Best_must_try_2.0/best14.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    //for(int i=1;i<=n;i=i+2){
+        //cout<<i<<" ";
+    //}
+    for(int i=1;i<=n;i=i+2){
+        cout<<i<<" ";
+    }
+    return 0;
+}
+//sample Input 10
+//Output: 1 3 5 7 9
