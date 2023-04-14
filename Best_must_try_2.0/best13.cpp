246bdcfc177a03cea8f13036513946f4433a1561
Ayush Siloiya <85118674+ayushsiloiya619@users.noreply.github.com>
2023-04-14 08:36:43 +0530

    file

---
+++ Best_must_try_2.0/best13.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int day;
+    cin>>day;
+    switch(day){
+        case 1:
+            cout<<"Mon"<<endl;
+            break;
+        case 2:
+            cout<<"Tue"<<endl;
+            break;
+        case 3:
+            cout<<"wed"<<endl;
+            break;
+        case 4:
+            cout<<"Thur"<<endl;
+            break;
+        case 5:
+            cout<<"Fri"<<endl;
+            break;
+        case 6:
+            cout<<"Sat"<<endl;
+            break;
+        default:
+            cout<<"Sun"<<endl;
+            break;
+    }
+    return 0;
+}
+//Swicth case revision.
+//Cool bane ki technique.
